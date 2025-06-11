5936ce6a996c6c66a0919cbe38588c40169c54d2
Tasmia Chowdhury <tasmiachowdhury123@gmail.com>
2025-06-11 18:07:49 -0400

    clear button

---
+++ script.js
@@ -0,0 +1,13 @@
+const screen = document.querySelector("#screen");
+const clear = document.querySelector("#clear");
+const add = document.querySelector("#add");
+const subtract = document.querySelector("#subtract");
+const multiply = document.querySelector("#multiply");
+const divide = document.querySelector("#division");
+const equal = document.querySelector("equal");
+
+clear.addEventListener("click", ()=>{
+   screen.textContent = "";
+});
+
+

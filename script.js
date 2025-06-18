7e7dba9192ed80fa403de59e570c12e26612e05a
Tasmia Chowdhury <tasmiachowdhury123@gmail.com>
2025-06-17 20:53:35 -0400

    fixed bugs

--- script.js
+++ script.js
@@ -1,115 +1,97 @@
 const screen = document.querySelector("#screen");
 const clear = document.querySelector("#clear");
-// const add = document.querySelector("#add");
-// const subtract = document.querySelector("#subtract");
-// const multiply = document.querySelector("#multiply");
-// const divide = document.querySelector("#division");
-// const equal = document.querySelector("#equal");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector("#equal");
+
 let firstNumber = "";
-let firstOperator ="";
-let secondNumber ="";
+let firstOperator = "";
+let secondNumber = "";
 let secNumberInputted = false;
+let justCalculated = false;
 
-//clear button 
-clear.addEventListener("click", ()=>{
-   screen.textContent = "";
-   firstNumber ="";
-   secondNumber ="";
-   firstOperator="";
-   secNumberInputted = false;
-   console.log("cleared");
+// Clear button
+clear.addEventListener("click", () => {
+  screen.textContent = "";
+  firstNumber = "";
+  secondNumber = "";
+  firstOperator = "";
+  secNumberInputted = false;
+  justCalculated = false;
+  console.log("cleared");
 });
 
-//convert to num back to string and display
-function display_numbers(result){
-    firstNumber = result;
-    secondNumber = '';
-    secNumberInputted = false;
-    string_of_nums = result.toString();
-    screen.textContent = string_of_nums;
-};
+function display_numbers(result) {
+  firstNumber = result.toString();
+  secondNumber = "";
+  secNumberInputted = false;
+  screen.textContent = firstNumber;
+  justCalculated = true;
+}
 
-function adding(a,b){
-   let a_num = Number(a);
-   let b_num = Number(b);
-   let sumOfNums = a_num+b_num;
-   display_numbers(sumOfNums);
-};
+function operate(op, a, b) {
+  let a_num = Number(a);
+  let b_num = Number(b);
+  let result = 0;
 
-function subtracting(a,b){
-    let a_num = Number(a);
-   let b_num = Number(b);
-   let diffOfNums = a_num-b_num;
-   display_numbers(diffOfNums);
-};
+  switch (op) {
+    case "+":
+      result = a_num + b_num;
+      break;
+    case "-":
+      result = a_num - b_num;
+      break;
+    case "*":
+      result = a_num * b_num;
+      break;
+    case "/":
+      result = Math.round((a_num / b_num) * 1e7) / 1e7;
+      break;
+  }
 
-function roundToDecimal(number, decimalPlaces) {
-  const multiplier = Math.pow(10, decimalPlaces);
-  return Math.round(number * multiplier) / multiplier;
-};
-function dividing(a,b){
-    let a_num = Number(a);
-    let b_num = Number(b);
-    let qoute = roundToDecimal((a/b), 7);
-    display_numbers(qoute);
-};
+  display_numbers(result);
+}
 
-function multipling(a,b){
-    let a_num = Number(a);
-    let b_num = Number(b);
-    let product = a_num*b_num;
-    display_numbers(product);
-};
+function populateDisplay(e) {
+  if (justCalculated && !secNumberInputted) {
+    // Start fresh only if no operator was selected
+    screen.textContent = "";
+    firstNumber = "";
+    justCalculated = false;
+  }
 
-function operate(op, a, b){
-    if(op==='+'){
-        adding(a,b);
-    }
-    else if(op==='-'){
-        subtracting(a,b);
-    }
-    else if(op==='/'){
-        dividing(a,b);
-    }
-    else if(op==="*"){
-        multipling(a,b);
-    }
+  screen.textContent += e.target.textContent;
 
-};
+  if (!secNumberInputted) {
+    firstNumber += e.target.textContent;
+  } else {
+    secondNumber += e.target.textContent;
+  }
+}
 
-function populateDisplay(e){
-    screen.textContent += e.target.textContent;
-    if(!secNumberInputted){
-        firstNumber +=e.target.textContent;
-    }
-    else if(secNumberInputted){
-        secondNumber+=e.target.textContent;
-    }
-    if(secNumberInputted && secondNumber!=''){
-         operate(firstOperator, firstNumber, secondNumber);
-    }
-};
-
-numbers.forEach(button =>{
-    button.addEventListener("click", populateDisplay);
+numbers.forEach((button) => {
+  button.addEventListener("click", populateDisplay);
 });
 
-function storeOp(e){
-    firstOperator=e.target.textContent;
-    screen.textContent="";
-    secNumberInputted= true;
-};
+function storeOp(e) {
+  if (firstOperator && secNumberInputted && secondNumber !== "") {
+    operate(firstOperator, firstNumber, secondNumber);
+  }
 
-operators.forEach(button =>{
-    button.addEventListener("click", storeOp); 
+  firstOperator = e.target.textContent;
+  secNumberInputted = true;
+  justCalculated = false;
+  screen.textContent = "";
+}
+
+operators.forEach((button) => {
+  button.addEventListener("click", storeOp);
 });
 
-
-equal.addEventListener("click",()=>{
-    if(secNumberInputted){
-        operate(firstOperator, firstNumber, secondNumber);
-    }
+equal.addEventListener("click", () => {
+  if (secNumberInputted && secondNumber !== "") {
+    operate(firstOperator, firstNumber, secondNumber);
+    firstOperator = "";
+    justCalculated = true;
+  }
 });
-
